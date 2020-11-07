6de58984fd8f9ee4043d074845374387d5fb5d3c
Mark Longchamps <mark_longchamps@MarkLonsMacBook.home>
2020-11-07 10:07:12 -0500

    DID NOT ADD PHOTOS but fixed the script index page

--- app/javascript/react/components/ScriptTile.js
+++ app/javascript/react/components/ScriptTile.js
@@ -1,3 +1,37 @@
 import React from "react"
+import { Link } from 'react-router-dom'
 
-cons
+const ScriptTile = (props) => {
+
+return(
+  <div>
+    <Link to={`/scripts/${props.script_id}`}>
+    <li>PROMO NAME: {props.name_of_promo}</li>
+    </Link>
+    DESCRIPTION: {props.name_of_promo}<br></br>
+    CREATED ON: {(new Date (props.created_at)).toLocaleDateString()}
+    <br></br>
+    <br></br>
+    
+    
+    </div>
+
+)
+
+
+
+}
+
+
+export default ScriptTile
+
+
+    //     <div key={script.id}>
+    //         <Link to={`/scripts/${script.id}`}>
+    //           <li>
+    //           {script.name_of_promo} *  * {script.description} ||Creation Date {(new Date (script.created_at)).toLocaleDateString()}
+    //           </li>
+    //         </Link>
+    //     </div>
+    //   )
+    // })
--- app/javascript/react/components/ScriptsIndexPage.js
+++ app/javascript/react/components/ScriptsIndexPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { Link } from 'react-router-dom'
 import ScriptFormContainer from "./ScriptFormContainer"
+import ScriptTile from "./ScriptTile"
 
 const ScriptsIndexPage = (props) => {
   const [scripts, setScripts] = useState([])
@@ -61,14 +62,12 @@ const ScriptsIndexPage = (props) => {
   
   const scriptListItems = scripts.map((script) => {
    
-    return (
-      <div key={script.id}>
-          <Link to={`/scripts/${script.id}`}>
-            <li>
-            {script.name_of_promo} *  * {script.description} ||Creation Date {(new Date (script.created_at)).toLocaleDateString()}
-            </li>
-          </Link>
-      </div>
+    return ( <ScriptTile
+      script_id={script.id}
+      name_of_promo={script.name_of_promo}
+      description={script.description}
+      created_at={script.created_at}
+      />
     )
   })
 
@@ -90,6 +89,110 @@ const ScriptsIndexPage = (props) => {
 }
 
 export default ScriptsIndexPage
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useEffect, useState } from "react"
+// import { Link } from 'react-router-dom'
+// import ScriptFormContainer from "./ScriptFormContainer"
+
+// const ScriptsIndexPage = (props) => {
+//   const [scripts, setScripts] = useState([])
+  
+//   const id = props.match.params.id
+ 
+//   useEffect(() => {
+//     fetch("/api/v1/scripts.json")
+//     .then (response => {
+//       if (response.ok) {
+//         return response
+//       } else {
+//         let errorMessage = `${response.status} (${response.statusText})`,
+//           error = new Error(errorMessage)
+//         throw error
+//       }
+//     })
+//     .then(response => response.json())
+//     .then(body => {
+//       setScripts(body.scripts) 
+      
+//     })
+//     .catch(error => console.error(`Error in fetch: ${error.message}`))
+    
+//   }, [])
+
+//   const addNewScriptFunction = (newScriptObject) => {
+    
+//     fetch(`/api/v1/scripts.json`, {
+//       method: "POST",
+//       body: JSON.stringify(newScriptObject),
+//       credentials: "same-origin",
+//         headers: {
+//           'Accept': 'application/json',
+//           'Content-Type': 'application/json'
+//         }
+//     })
+//     .then (response => {
+//     if (response.ok) {
+//       return response
+//     } else {
+//       let errorMessage = `${response.status} (${response.statusText})`,
+//         error = new Error(errorMessage)
+//       throw error
+//     }
+//   })
+//   .then(response => response.json())
+//   .then(body => {
+//     setScripts([...scripts, body.script])
+    
+    
+    
+    
+//   })
+//   .catch(error => console.error(`Error in fetch: ${error.message}`))
+  
+//   }
+  
+//   const scriptListItems = scripts.map((script) => {
+   
+//     return (
+//       <div key={script.id}>
+//           <Link to={`/scripts/${script.id}`}>
+//             <li>
+//             {script.name_of_promo} *  * {script.description} ||Creation Date {(new Date (script.created_at)).toLocaleDateString()}
+//             </li>
+//           </Link>
+//       </div>
+//     )
+//   })
+
+
+//   return (
+//     <div>
+//       <h1>Create a New Script</h1>
+//       <ScriptFormContainer addNewScriptFunction={addNewScriptFunction}
+//       />
+//       <br></br>
+//       <br></br>
+//       <h1>My Active Scripts</h1>
+//       <ol>
+//         {scriptListItems}
+//       </ol>
+      
+//     </div> 
+//   )
+// }
+
+// export default ScriptsIndexPage
 
 
 
